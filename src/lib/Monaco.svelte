31d51d2a97206ef30bb2323db30b12f9aa53fe7c
Felice Pollano <felice@felicepollano.com>
2022-07-01 13:45:25 +0200

    added markup property to hopefully server sude render

--- src/lib/Monaco.svelte
+++ src/lib/Monaco.svelte
@@ -8,33 +8,39 @@ const dispatch = createEventDispatcher();
 
 
 export let text;
+export let markup;
 export const save=()=>{
-    
+    markup = div.innerHTML;
+    console.log(markup);
 }
 let div;
-onMount( async ()=>{
-let monaco=await import('monaco-editor');    
-let editor = monaco.editor.create(div, {
-        value: text,
-        language: 'sql',
-        minimap: { enabled: false },
-            automaticLayout: true,
-            scrollBeyondLastLine: false
-    });
-    recalcHeight(editor,div);
-    editor.getModel().onDidChangeContent((event) => {
-                recalcHeight(editor,div);
-                dispatch('changed');
-               
-            });
-});
-let recalcHeight=(editor,div)=>{
+let editor;
+async function loadMonaco(){
+    if(!editor){
+    let monaco=await import('monaco-editor');    
+    editor = monaco.editor.create(div, {
+            value: text,
+            language: 'sql',
+            minimap: { enabled: false },
+                automaticLayout: true,
+                scrollBeyondLastLine: false
+        });
+        recalcHeight(editor,div);
+        editor.getModel().onDidChangeContent((event) => {
+                    recalcHeight(editor,div);
+                    dispatch('changed');
+                
+                });
+        }
+    }
+
+    let recalcHeight=(editor,div)=>{
     const contentHeight = editor.getModel().getLineCount() * 19 ;
     div.style.height=32+contentHeight+'px';
     editor.layout();
 }
 </script>
-<div class="monacocontainer" bind:this={div}>
+<div on:click={loadMonaco} class="monacocontainer" bind:this={div}>
     
 </div>
 <style>
@@ -42,5 +48,6 @@ let recalcHeight=(editor,div)=>{
         margin-top:-1px;
         border:1px dashed rgb(180,180,180);
         text-align: left;
+        min-height: 20px;
     }
 </style>
