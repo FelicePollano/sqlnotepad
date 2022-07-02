069f3c983d3bbd078f02ff422d0f9dff2271e3c0
Felice Pollano <felice@felicepollano.com>
2022-07-02 17:04:55 +0200

    modified footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,13 +1,12 @@
 <script>
 
 </script>
-<div class="footer">
+<footer class="footer">
     2022 Felice Pollano
-</div>
+</footer>
 <style>
     .footer{
-        margin-top: 10px;
-        text-align: center;
+        height: calc(100vh - 100px);
         background-color: rgba(0, 0, 0, .1);
         height: 32px;
         border-top: 1px solid gray
--- src/lib/Markdown.svelte
+++ src/lib/Markdown.svelte
@@ -8,18 +8,22 @@
         editing=false;
     }
     let editing=true;
+  
     function handleChanged(){
         dispatch('changed');
+    }
+    function enterEdit(){
+        editing=!editing;
     }
    
 </script>
 {#if editing}
-<div class="editorcontainer">
+<div class="editorcontainer" >
 <textarea width=100% bind:value={source}  on:input={handleChanged}></textarea>
 </div>
 {/if}
-<div class="markdowncontainer">
-<SvelteMarkdown {source}></SvelteMarkdown>
+<div class="markdowncontainer" on:click={enterEdit}>
+<SvelteMarkdown on:click={enterEdit} {source}></SvelteMarkdown>
 </div>
 
 
